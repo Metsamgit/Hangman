f2d8e0674ab87160b7ce79a93d744ea4e9f3a1fe
RemiBeis <remi.beis12@gmail.com>
2024-09-23 11:46:47 +0200

    win

--- main.go
+++ main.go
@@ -5,18 +5,21 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
-	file, err := os.Open("words.txt")
+
+	wordFile, err := os.Open("words.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer wordFile.Close()
 
 	var lines []string
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(wordFile)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
@@ -43,6 +46,7 @@ func main() {
 			RandomLetters = append(RandomLetters, r)
 		}
 	}
+
 	word := make([]rune, len(RandomWord))
 	for i := range word {
 		word[i] = '_'
@@ -52,19 +56,66 @@ func main() {
 	}
 	fmt.Println("Word to guess:", string(word))
 
-	var i int = 10
-	var test string
+	var attempts int = 9
+	var guess string
 	fmt.Println("Enter the word: ")
-	fmt.Scanln(&test)
-	for i > 0 {
-		if test == RandomWord {
+	fmt.Scanln(&guess)
+
+	stages := readHangmanStages("hangmanposition.txt")
+
+	for attempts > 0 {
+		if guess == RandomWord {
 			fmt.Println("Correct!")
 			break
 		} else {
-			fmt.Println("Incorrect!", i, "attempts left")
-			i--
+			fmt.Println("Incorrect!", attempts, "attempts left")
+			attempts--
+
+			if attempts < len(stages) {
+				fmt.Println(stages[len(stages)-attempts-1])
+			}
+
 			fmt.Println("Enter the word: ")
-			fmt.Scanln(&test)
+			fmt.Scanln(&guess)
 		}
 	}
+
+	if attempts == 0 {
+		fmt.Println("Game over! The word was:", RandomWord)
+	}
+}
+
+func readHangmanStages(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error reading hangman stage file:", err)
+		return nil
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var stages []string
+	var currentStage strings.Builder
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+
+			stages = append(stages, currentStage.String())
+			currentStage.Reset()
+		} else {
+
+			currentStage.WriteString(line + "\n")
+		}
+	}
+
+	if currentStage.Len() > 0 {
+		stages = append(stages, currentStage.String())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error scanning file:", err)
+	}
+
+	return stages
 }
